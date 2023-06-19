6be334e2545fd63da31dcefac7720ecf83e9497c
khchandn <khchandn@connect.ust.hk>
2023-06-19 15:01:18 +0800

    Revert "Revert "Create New Method for the duo person control panel""
    This reverts commit 6d100e3f5bf9cb9feacf40a6282b7e86ea42feb9.

--- Web files/controltemp.js
+++ Web files/controltemp.js
@@ -68,6 +68,7 @@ const config = { // config of the graph
     type: 'line',
     data: topHeartData,
     options: {
+      spanGaps: true,
       responsive: true,
       plugins: {
         legend: {
@@ -465,6 +466,7 @@ function toggleChart(options,chart){
         isPaused = true;
         clearInterval(intervalId); 
       }
+      
       display_time1 = time1;
       display_extractedheartdata1 = extractedheartdata1;
       display_time2 = time2;
